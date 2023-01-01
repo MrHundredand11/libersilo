51cea9ad503aae3e5f7ffebda8920cf01ea7ed0c
MrHundredand11 <49173798+MrHundredand11@users.noreply.github.com>
2023-01-01 11:45:20 -0600

    Update trunking.js

--- ls/000/code/js/trunking.js
+++ ls/000/code/js/trunking.js
@@ -28,7 +28,7 @@ function shovelDigging(scion_id) {
             break;
 
         case 'styl_shvl_s1':
-            document.getElementById('id_b_p_' + scion_id).setAttribute('class', 'styl_shvl_s2');
+            document.getElementById(`id_b_p_${scion_id}`).setAttribute('class', 'styl_shvl_s2');
             var testyy = document.getElementById(`id_b_trstlbrd_${scion_id}`);
             console.log(testyy);
             console.log(testyy.querySelectorAll(`id_b_trstlbrd_${scion_id} > *`));
@@ -58,21 +58,21 @@ function showHideStem(scion_id) {
         document.getElementById(`id_stm_bmp_top_${scion_id}`).style.display = "block";
         document.getElementById(`id_stm_bmp_btm_${scion_id}`).style.display = "block";
         document.getElementById(`id_stm_trstlbrd_${scion_id}`).style.display = "block";
-        document.getElementById('id_stm_cntnr_' + scion_id).classList.add('stt_show');
-        document.getElementById('id_stm_cntnr_' + scion_id).classList.remove('stt_hide');
+        document.getElementById(`id_stm_cntnr_${scion_id}`).classList.add('stt_show');
+        document.getElementById(`id_stm_cntnr_${scion_id}`).classList.remove('stt_hide');
     }
     
 
 }
 
 function showHideTwig(scion_id) {
-    var twg_clss = document.getElementById("id_twg_cntnr_" + scion_id).className;
+    var twg_clss = document.getElementById(`id_twg_cntnr_${scion_id}`).className;
     if (twg_clss.includes('stt_show')) {
-        document.getElementById('id_twg_bmp_top_' + scion_id).style.display = "none";
-        document.getElementById('id_twg_bmp_btm_' + scion_id).style.display = "none";
-        document.getElementById('id_twg_cntn_' + scion_id).style.display = "none";
-        document.getElementById('id_twg_cntnr_' + scion_id).classList.add('stt_hide');
-        document.getElementById('id_twg_cntnr_' + scion_id).classList.remove('stt_show');
+        document.getElementById(`id_twg_bmp_top_${scion_id}`).style.display = "none";
+        document.getElementById(`id_twg_bmp_btm_${scion_id}`).style.display = "none";
+        document.getElementById(`id_twg_cntn_${scion_id}`).style.display = "none";
+        document.getElementById(`id_twg_cntnr_${scion_id}`).classList.add('stt_hide');
+        document.getElementById(`id_twg_cntnr_${scion_id}`).classList.remove('stt_show');
     } else if (twg_clss.includes('stt_hide')) {
         document.getElementById('id_twg_bmp_top_' + scion_id).style.display = "block";
         document.getElementById('id_twg_bmp_btm_' + scion_id).style.display = "block";

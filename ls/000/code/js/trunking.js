83b58bac597d31376966d35d2b4668fd319e34f7
MrHundredand11 <49173798+MrHundredand11@users.noreply.github.com>
2023-05-27 08:13:15 -0500

    Update trunking.js

--- ls/000/code/js/trunking.js
+++ ls/000/code/js/trunking.js
@@ -201,9 +201,9 @@ function gen_Insc(insc_i, scion) {
                             stm_expnd.appendChild(twg_cntnr);
 
                             for (i2 in scion.scions[lfl].scions) {
-                                //console.log(lf.scions[lfl].scions[i2]);
+                                console.log(lf.scions[lfl].scions[i2]);
                                 var entr_full = gen_Insc(i2, scion.scions[lfl].scions[i2]);
-                                //console.log(entr_full);
+                                console.log(entr_full);
                                 twg_cntn.appendChild(entr_full);
                             }
                         }

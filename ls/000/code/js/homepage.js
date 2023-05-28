74fbc5e4a34384592ba1aee71db2ba0e9fecac09
MrHundredand11 <49173798+MrHundredand11@users.noreply.github.com>
2023-05-28 07:28:22 -0500

    Update homepage.js

--- ls/000/code/js/homepage.js
+++ ls/000/code/js/homepage.js
@@ -88,21 +88,7 @@ function homeFetch() {
                 headBoard.appendChild(ls_ttl);
                 trstle_etchngs.appendChild(headBoard);
             }
-            populatePage(data)
-
-            function popDir(data) {
-                let drct_rcrd = data.scions
-                var drct_main = createElementWithClass('div', 'branch_container');
-                var drct_head = createElementWithClass('div', 'styl_drct_head');
-                var drct_cntn = gen_ECI('div', 'styl_drct_cntn', 'id_drct_cntn');
-                drct_head.innerHTML = "<span class='styl_pointers'>-></span> <strong><u>Full Directory to Liber Silo</u></strong>:";
-                drct_main.append(drct_head, drct_cntn);
-                trstle_etchngs.appendChild(drct_main);
-                for (x in drct_rcrd) {
-                    gen_dir(drct_rcrd[x]);
-                }
-            }
-            popDir(data);
+            populatePage(data);
         })
 
         .catch(err => {

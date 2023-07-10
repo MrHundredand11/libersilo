73bccac898596034a29a3cf7b230bb2727110f58
MrHundredand11 <49173798+MrHundredand11@users.noreply.github.com>
2023-07-10 07:22:40 -0500

    Update trunking.js

--- ls/000/code/js/trunking.js
+++ ls/000/code/js/trunking.js
@@ -241,7 +241,7 @@ function gen_Insc(insc_i, scion) {
 
                 case 'quot':
                     var insc_cntn = createElementWithClass('div', 'styl_insc_quot');
-                    insc_cntn.innerHTML = scion.insc_cntn;
+                    insc_cntn.innerHTML = `<span class='bknd_01'>[</span>\t${scion.insc_cnt}<span class='bknd_01'>]</span>`;
                     insc_prtr.append(insc_pntr, insc_cntn);
                     break;
 

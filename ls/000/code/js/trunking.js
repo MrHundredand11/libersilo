f84f7be6f91f117a7bb2e59d0fc949fba1f788ee
MrHundredand11 <49173798+MrHundredand11@users.noreply.github.com>
2023-07-25 11:20:47 -0500

    Update trunking.js

--- ls/000/code/js/trunking.js
+++ ls/000/code/js/trunking.js
@@ -365,17 +365,15 @@ function gen_Insc(insc_i, scion) {
                     var in_tbl = gen_ECI('table', 'tbl_cntnr', `id_tbl_${scion.scion_id}`);
                     let thead = in_tbl.createTHead();
                     let row = thead.insertRow();
-                    console.log(scion.tabl_keys);
-                    console.log(scion.tabl_keys.ky_head);
-                    for (let key of scion.tabl_keys.ky_head) {
-                                let th = document.createElement("th");
-                                th.innerHTML = key.ky_head[key];
-                                for (let ent of scion.tabl_keys.ky_rows) {
-                                    let text = document.createTextNode(ent[key]);
-                                    th.appendChild(text);
-                                }
-                                row.appendChild(th);
-                            }
+                    for (let hd of scion.tabl_head) {
+                        let th = document.createElement("th");
+                        th.innerHTML = scion.tabl_head[hd];
+                        for (let ent of scion.tabl_keys) {
+                            let text = document.createTextNode(scion.tabl_keys[ent][hd]);
+                            th.appendChild(text);
+                        }
+                        row.appendChild(th);
+                    }
                     return in_tbl;
             }
 

ec9878737f43ffc2ccecfec802c93f825194ffac
MrHundredand11 <49173798+MrHundredand11@users.noreply.github.com>
2023-08-01 10:02:16 -0500

    Update trunking.js

--- ls/000/code/js/trunking.js
+++ ls/000/code/js/trunking.js
@@ -516,9 +516,9 @@ function gen_Insc(insc_i, scion) {
                     }
                     
                     if (scion.cite_site !== undefined) {
-                            var cite_site = gen_ECI('div', 'cite_site', `id_cite_site_${x}_${scion.scion_id}`);
-                            cite_site.innerHTML = `<span class='cite_flow'>|\\_>> <em>[Web Site]</em>::</span>&nbsp;<span class='cite_sbdt'>${scion.cite_site}</span>`;
-                            cite_scns.append(cite_site);
+                        var cite_site = gen_ECI('div', 'cite_site', `id_cite_site_${scion.scion_id}`);
+                        cite_site.innerHTML = `<span class='cite_flow'>|\\_>> <em>[Web Site]</em>::</span>&nbsp;<span class='cite_sbdt'>${scion.cite_site}</span>`;
+                        cite_scns.append(cite_site);
                     }
 
                     if (scion.cite_auth !== undefined) {
